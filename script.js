40eadaac7ba189c55727d9ae01d39bbdbac743e3
Marti <marti.iden@gmail.com>
2023-04-30 18:30:46 +0700

    refactor:сhanged approach for special buttons

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const ButtonsRus = {
-  Backquote: 'ё', Digit1: '1', Digit2: '2', Digit3: '3', Digit4: '4', Digit5: '5', Digit6: '6', Digit7: '7', Digit8: '8', Digit9: '9', Digit0: '0', Minus: '-', Equal: '=', Backspace: 'Backspace', Tab: 'Tab', KeyQ: 'й', KeyW: 'ц', KeyE: 'у', KeyR: 'к', KeyT: 'е', KeyY: 'н', KeyU: 'г', KeyI: 'ш', KeyO: 'щ', KeyP: 'з', BracketLeft: 'х', BracketRight: 'ъ', Backslash: '\\', CapsLock: 'CapsLock', KeyA: 'ф', KeyS: 'ы', KeyD: 'в', KeyF: 'а', KeyG: 'п', KeyH: 'р', KeyJ: 'о', KeyK: 'л', KeyL: 'д', Semicolon: 'ж', Quote: 'э', Enter: '\n', ShiftLeft: 'Shift', KeyZ: 'я', KeyX: 'ч', KeyC: 'с', KeyV: 'м', KeyB: 'и', KeyN: 'т', KeyM: 'ь', Comma: 'б', Period: 'ю', Slash: '.', ArrowUp: '▲', ShiftRight: 'Shift', ControlLeft: 'CTRL', MetaLeft: 'Win', AltLeft: 'Alt', Space: ' ', AltRight: 'Alt', ControlRight: 'CTRL', ArrowLeft: '◄', ArrowDown: '▼', ArrowRight: '►',
+  Backquote: 'ё', Digit1: '1', Digit2: '2', Digit3: '3', Digit4: '4', Digit5: '5', Digit6: '6', Digit7: '7', Digit8: '8', Digit9: '9', Digit0: '0', Minus: '-', Equal: '=', Backspace: 'BACKSPACE', Tab: 'TAB', KeyQ: 'й', KeyW: 'ц', KeyE: 'у', KeyR: 'к', KeyT: 'е', KeyY: 'н', KeyU: 'г', KeyI: 'ш', KeyO: 'щ', KeyP: 'з', BracketLeft: 'х', BracketRight: 'ъ', Backslash: '\\', CapsLock: 'CAPSLOCK', KeyA: 'ф', KeyS: 'ы', KeyD: 'в', KeyF: 'а', KeyG: 'п', KeyH: 'р', KeyJ: 'о', KeyK: 'л', KeyL: 'д', Semicolon: 'ж', Quote: 'э', Enter: 'ENTER', ShiftLeft: 'SHIFT', KeyZ: 'я', KeyX: 'ч', KeyC: 'с', KeyV: 'м', KeyB: 'и', KeyN: 'т', KeyM: 'ь', Comma: 'б', Period: 'ю', Slash: '.', ArrowUp: '▲', ShiftRight: 'SHIFT', ControlLeft: 'CTRL', MetaLeft: 'WIN', AltLeft: 'ALT', Space: ' ', AltRight: 'ALT', ControlRight: 'CTRL', ArrowLeft: '◄', ArrowDown: '▼', ArrowRight: '►',
 };
 const ButtonsEng = {
-  Backquote: '`', Digit1: '1', Digit2: '2', Digit3: '3', Digit4: '4', Digit5: '5', Digit6: '6', Digit7: '7', Digit8: '8', Digit9: '9', Digit0: '0', Minus: '-', Equal: '=', Backspace: 'Backspace', Tab: 'Tab', KeyQ: 'q', KeyW: 'w', KeyE: 'e', KeyR: 'r', KeyT: 't', KeyY: 'y', KeyU: 'u', KeyI: 'i', KeyO: 'o', KeyP: 'p', BracketLeft: '[', BracketRight: ']', Backslash: '\\', CapsLock: 'CapsLock', KeyA: 'a', KeyS: 's', KeyD: 'd', KeyF: 'f', KeyG: 'g', KeyH: 'h', KeyJ: 'j', KeyK: 'k', KeyL: 'l', Semicolon: ';', Quote: "'", Enter: '\n', ShiftLeft: 'Shift', KeyZ: 'z', KeyX: 'x', KeyC: 'c', KeyV: 'v', KeyB: 'b', KeyN: 'n', KeyM: 'm', Comma: ',', Period: '.', Slash: '/', ArrowUp: '▲', ShiftRight: 'Shift', ControlLeft: 'CTRL', MetaLeft: 'Win', AltLeft: 'Alt', Space: ' ', AltRight: 'Alt', ControlRight: 'CTRL', ArrowLeft: '◄', ArrowDown: '▼', ArrowRight: '►',
+  Backquote: '`', Digit1: '1', Digit2: '2', Digit3: '3', Digit4: '4', Digit5: '5', Digit6: '6', Digit7: '7', Digit8: '8', Digit9: '9', Digit0: '0', Minus: '-', Equal: '=', Backspace: 'BACKSPACE', Tab: 'TAB', KeyQ: 'q', KeyW: 'w', KeyE: 'e', KeyR: 'r', KeyT: 't', KeyY: 'y', KeyU: 'u', KeyI: 'i', KeyO: 'o', KeyP: 'p', BracketLeft: '[', BracketRight: ']', Backslash: '\\', CapsLock: 'CAPSLOCK', KeyA: 'a', KeyS: 's', KeyD: 'd', KeyF: 'f', KeyG: 'g', KeyH: 'h', KeyJ: 'j', KeyK: 'k', KeyL: 'l', Semicolon: ';', Quote: "'", Enter: 'ENTER', ShiftLeft: 'SHIFT', KeyZ: 'z', KeyX: 'x', KeyC: 'c', KeyV: 'v', KeyB: 'b', KeyN: 'n', KeyM: 'm', Comma: ',', Period: '.', Slash: '/', ArrowUp: '▲', ShiftRight: 'SHIFT', ControlLeft: 'CTRL', MetaLeft: 'WIN', AltLeft: 'ALT', Space: ' ', AltRight: 'ALT', ControlRight: 'CTRL', ArrowLeft: '◄', ArrowDown: '▼', ArrowRight: '►',
 };
 
 let caps = false;
@@ -15,36 +15,19 @@ body.insertAdjacentHTML('afterbegin', '<main class="main"><div class="wrapper"><
 const textarea = document.querySelector('textarea');
 const keyboard = document.querySelector('.keyboard');
 
-function languageСhange() {
-  document.addEventListener('keydown', (event) => {
-    if (event.shiftKey && event.altKey) {
-      if (lang === 'rus') {
-        lang = 'eng';
-      } else {
-        lang = 'rus';
-      }
-    }
-  });
-}
-
-function capsСhange() {
-  document.addEventListener('keydown', (event) => {
-    if (event.code === 'CapsLock') {
-      if (caps === false) {
-        caps = true;
-      } else {
-        caps = false;
-      }
-    }
-  });
-}
-
-capsСhange();
-languageСhange();
-
-Object.keys(ButtonsRus).forEach((key) => {
+function initKeyBoard(key) {
   const div = document.createElement('div');
-  div.innerHTML = ButtonsRus[key];
+  if (lang === 'rus') {
+    if (caps === true) {
+      div.innerHTML = ButtonsRus[key].toUpperCase();
+    } else {
+      div.innerHTML = ButtonsRus[key];
+    }
+  } else if (caps === true) {
+    div.innerHTML = ButtonsEng[key].toUpperCase();
+  } else {
+    div.innerHTML = ButtonsEng[key];
+  }
   keyboard.append(div);
   div.classList.add(key);
   div.classList.add('keyboard__key');
@@ -60,6 +43,75 @@ Object.keys(ButtonsRus).forEach((key) => {
   if (key === 'Space') {
     div.classList.add('keyboard__key_space');
   }
+}
+
+function languageСhange() {
+  document.addEventListener('keydown', (event) => {
+    if (event.shiftKey && event.altKey) {
+      if (lang === 'rus') {
+        lang = 'eng';
+      } else {
+        lang = 'rus';
+      }
+      keyboard.innerHTML = '';
+      Object.keys(ButtonsRus).forEach((key) => {
+        initKeyBoard(key);
+      });
+    }
+  });
+}
+
+function capsСhange() {
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'CapsLock') {
+      if (caps === false) {
+        caps = true;
+      } else {
+        caps = false;
+      }
+      keyboard.innerHTML = '';
+      Object.keys(ButtonsRus).forEach((key) => {
+        initKeyBoard(key);
+      });
+    }
+  });
+  keyboard.addEventListener('mousedown', (event) => {
+    if (event.target.classList.contains('keyboard__key')) {
+      if (event.target.textContent === 'CAPSLOCK') {
+        if (caps === false) {
+          caps = true;
+        } else {
+          caps = false;
+        }
+      }
+    }
+    keyboard.innerHTML = '';
+    Object.keys(ButtonsRus).forEach((key) => {
+      initKeyBoard(key);
+    });
+  });
+}
+
+function backspaceFunc() {
+  if (textareaPosition <= 0) {
+    textareaPosition = textarea.selectionStart;
+  }
+  let txtemp = textarea.value;
+  txtemp = txtemp.slice(0, textareaPosition - 1) + txtemp.slice(textareaPosition, txtemp.length);
+  textarea.value = txtemp;
+  console.log(textareaPosition);
+  textareaPosition -= 1;
+}
+
+function enterFunc() {
+  textarea.value += '\n';
+}
+
+capsСhange();
+languageСhange();
+
+Object.keys(ButtonsRus).forEach((key) => {
+  initKeyBoard(key);
 });
 
 document.addEventListener('keydown', (event) => {
@@ -69,14 +121,12 @@ document.addEventListener('keydown', (event) => {
       Object.keys(ButtonsRus).forEach((key) => {
         if (event.code === key) {
           if (event.code === 'Backspace') {
-            if (textareaPosition <= 0) {
-              textareaPosition = textarea.selectionStart;
-            }
-            textarea.value = textarea.value.slice(0, textareaPosition - 1) + textarea.value.slice(textareaPosition, textarea.value.length);
-            console.log(textareaPosition);
-            textareaPosition -= 1;
+            backspaceFunc();
           }
-          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace')) {
+          if (event.code === 'Enter') {
+            enterFunc();
+          }
+          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace' || event.code === 'Enter')) {
             textarea.value += ButtonsRus[`${key}`];
           }
         }
@@ -86,9 +136,12 @@ document.addEventListener('keydown', (event) => {
       Object.keys(ButtonsRus).forEach((key) => {
         if (event.code === key) {
           if (event.code === 'Backspace') {
-            textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+            backspaceFunc();
           }
-          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace')) {
+          if (event.code === 'Enter') {
+            enterFunc();
+          }
+          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace' || event.code === 'Enter')) {
             textarea.value += ButtonsRus[`${key}`].toUpperCase();
           }
         }
@@ -100,9 +153,12 @@ document.addEventListener('keydown', (event) => {
       Object.keys(ButtonsEng).forEach((key) => {
         if (event.code === key) {
           if (event.code === 'Backspace') {
-            textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+            backspaceFunc();
           }
-          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace')) {
+          if (event.code === 'Enter') {
+            enterFunc();
+          }
+          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace' || event.code === 'Enter')) {
             textarea.value += ButtonsEng[`${key}`];
           }
         }
@@ -112,9 +168,12 @@ document.addEventListener('keydown', (event) => {
       Object.keys(ButtonsEng).forEach((key) => {
         if (event.code === key) {
           if (event.code === 'Backspace') {
-            textarea.value = textarea.value.slice(0, textarea.value.length - 1);
+            backspaceFunc();
           }
-          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace')) {
+          if (event.code === 'Enter') {
+            enterFunc();
+          }
+          if (!(event.shiftKey || event.altKey || event.ctrlKey || event.metaKey || event.code === 'CapsLock' || event.code === 'Backspace' || event.code === 'Enter')) {
             textarea.value += ButtonsEng[`${key}`].toUpperCase();
           }
         }
@@ -125,12 +184,20 @@ document.addEventListener('keydown', (event) => {
 
 document.addEventListener('click', () => {
   textareaPosition = textarea.selectionStart;
-  console.log(textareaPosition);
 });
 
 keyboard.addEventListener('mousedown', (event) => {
   if (event.target.classList.contains('keyboard__key')) {
-    console.log(event.code);
-    textarea.value += event.target.textContent;
+    console.log(event.target);
+    if (!(event.target.textContent === 'SHIFT' || event.target.textContent === 'ALT' || event.target.textContent === 'CTRL' || event.target.textContent === 'TAB' || event.target.textContent === 'CAPSLOCK' || event.target.textContent === 'BACKSPACE' || event.target.textContent === 'WIN' || event.target.textContent === 'ENTER')) {
+      textarea.value += event.target.textContent;
+      textareaPosition = textarea.selectionStart;
+    }
+    if (event.target.textContent === 'BACKSPACE') {
+      backspaceFunc();
+    }
+    if (event.target.textContent === 'ENTER') {
+      enterFunc();
+    }
   }
 });
